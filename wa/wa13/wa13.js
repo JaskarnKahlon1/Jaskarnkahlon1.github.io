2183257b3fc6443478bc182444f0e579ca6ef0aa
JaskarnKahlon1 <97628488+JaskarnKahlon1@users.noreply.github.com>
2023-04-24 00:46:05 -0600

    wa13

--- wa/wa13/wa13.js
+++ wa/wa13/wa13.js
@@ -43,67 +43,36 @@ const output = document.querySelector('.output');
 let outputInt = parseInt(output.textContent);
 console.log(outputInt);
 
-const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
-const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
 const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
-const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+// const randomButton = document.querySelector('.random-button').addEventListener('click', random);
 const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+// const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+// const plusButton = document.querySelector('.plusButton').addEventListener('click', plus);
 
+const plusButton = document.getElementById('plusButton');
 
+function moveButtonPlus() {
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
+    plusButton.style.left = `${x}px`;
+    plusButton.style.top = `${y}px`;
+    if (outputInt < 100) {
+        outputInt +=5;
+        output.textContent = outputInt;
+        }
+}
 
-// const slider = document.getElementById('slider');
-// const optionsDiv = document.getElementById('options');
-// const options = optionsDiv.querySelectorAll('button');
-// slider.addEventListener('click', function(event) {
-//     optionsDiv.style.display = 'block';
-//     options.forEach(option => {
-//       option.addEventListener('click', function() {
-//         slider.value = value;
-//         outputInt = slider.value;
-//           output.textContent = outputInt;
+const minusButton = document.getElementById('minusButton');
 
-//         optionsDiv.style.display = 'none';
-//       });
-//     });
-//   });
-
-
-
-// // Get the canvas element
-const canvas = document.querySelector('canvas');
-
-// Set the width and height of the canvas
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-const context = canvas.getContext('2d');
-
-let x = 0;
-let y = 0;
-let num = 0;
-
-
-
-
-canvas.addEventListener('click', event => {
-  x = event.clientX;
-  y = event.clientY;
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    const color = `rgb(${r}, ${g}, ${b})`;
-
-  context.fillStyle = color;
-  context.beginPath();
-  context.arc(x, y, 10, 0, 2 * Math.PI);
-  context.fill();
-  num++;
-  console.log(num);
-  outputInt = num;
-  output.textContent = outputInt;
-
-
-});
+function moveButtonMinus() {
+    const x = Math.floor(Math.random() * window.innerWidth);
+    const y = Math.floor(Math.random() * window.innerHeight);
+    minusButton.style.left = `${x}px`;
+    minusButton.style.top = `${y}px`;
+    if (outputInt > 0) {
+        outputInt -= 1;
+        output.textContent = outputInt; }
+}
 
 
 
